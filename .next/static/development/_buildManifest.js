71ad11b9411b8838abe85160b410dcfd8a2f04eb
Hugo Esteban Quinteros Bustos <76698088+HQB92@users.noreply.github.com>
2024-06-15 18:22:23 -0400

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/customers":["static\u002Fchunks\u002Fpages\u002Fcustomers.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcustomers"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
