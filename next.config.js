83f05cca09379e64eadc49ae9ca03561feb4881d
Hugo Esteban Quinteros Bustos <76698088+HQB92@users.noreply.github.com>
2024-06-15 18:27:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-
-const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
-  output: 'export',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-  reactStrictMode: true,
-  basePath: isProd ? '/frontend-impch-zanartu' : '',
-  distDir: '/dist',
-};
+  reactStrictMode: true
+}
+
