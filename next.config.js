e0808c689f3d2fa5ddb07b356796b1f944cfb908
Hugo Esteban Quinteros Bustos <76698088+HQB92@users.noreply.github.com>
2024-07-18 13:31:55 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const withGraphql = require('next-plugin-graphql');
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['example.com'],
-    path: '/_next/assets', // Valor corregido para .images.path
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
